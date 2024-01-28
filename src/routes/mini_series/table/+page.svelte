1c7cd1dbfe7c34f67b0d9153b76a2688496d6d7d
DinoJay <jott88@googlemail.com>
2024-01-28 21:26:12 +0100

    changed price

--- src/routes/mini_series/table/+page.svelte
+++ src/routes/mini_series/table/+page.svelte
@@ -15,14 +15,14 @@
 	];
 </script>
 
-<div class=" p-3  md:p-12 items-center md:items-start">
+<div class=" p-3 md:p-12 items-center md:items-start">
 	<div class="">
 		<h1 class="text-3xl mb-1">Marée basse</h1>
 		<h2 class="mb-2">Table basse</h2>
-		<div class="text-justify text-sm text-gray-600 ">
+		<div class="text-justify text-sm text-gray-600">
 			<p class="">Gravure des formes à la CNC, marqueterie de laiton</p>
 			<p class="">Multiplex bouleau - hêtre - epoxy - laiton</p>
-			<p>280€/table - dispo: 3</p>
+			<p>500€/table - dispo: 3</p>
 		</div>
 	</div>
 	<div class="flex flex-col md:flex-row md:mt-12">
@@ -32,7 +32,7 @@
 			{/each}
 		</div>
 		<img
-			class="mini-series-table-mobile md:mini-series-table-desktop order-0 md:order-last mb-3 md:mb-0 max-w-series "
+			class="mini-series-table-mobile md:mini-series-table-desktop order-0 md:order-last mb-3 md:mb-0 max-w-series"
 			src="/table_plan.webp"
 		/>
 	</div>
