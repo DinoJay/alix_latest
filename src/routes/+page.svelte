1b7b9bf9ebec9cbd13b2b7818c6fafd2c34383e3
DinoJay <jott88@googlemail.com>
2024-04-17 21:28:13 +0200

    yeah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,9 @@
 	};
 </script>
 
-<div class="flex gap-4 md:p-0 p-3 overflow-auto">
+<div class="flex flex-col lg:flex-row gap-4 md:p-0 p-3 overflow-auto">
 	<a
-		class=" flex flex-col bg-cover flex-1 lg:home-el-width"
+		class=" flex flex-col bg-cover lg:flex-1 lg:home-el-height"
 		href="/creations"
 		style:background-image={'url("creations.webp")'}
 	>
@@ -18,7 +18,7 @@
 		</div>
 	</a>
 	<a
-		class=" flex flex-col bg-cover lg:home-el-width flex-1"
+		class=" flex flex-col bg-cover lg:home-el-height lg:flex-1"
 		href="/mini_series"
 		style:background-image={'url("decorsardine.webp")'}
 	>
@@ -27,7 +27,7 @@
 			<span class="hidden sm:block">Productions Multiples</span>
 		</div>
 	</a>
-	<a class=" flex flex-col coin bg-fill lg:home-el-width flex-1" href="/coin">
+	<a class=" flex flex-col coin bg-fill lg:home-el-height lg:flex-1" href="/coin">
 		<div class=" collab-text m-auto font-sans uppercase text-center text-white">
 			<span class="sm:block">Le Coin</span>
 		</div>
